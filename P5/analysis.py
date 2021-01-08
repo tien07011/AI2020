443597630d085cb962b214d26288ed867e9b22a0
tien07011 <56587599+tien07011@users.noreply.github.com>
2021-01-08 07:25:56 +0700

    Update analysis.py

--- P5/analysis.py
+++ P5/analysis.py
@@ -1,19 +1,3 @@
-# analysis.py
-# -----------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and Pieter 
-# Abbeel in Spring 2013.
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
-
-######################
-# ANALYSIS QUESTIONS #
-######################
-
-# Set the given parameters to obtain the specified policies through
-# value iteration.
 
 def question2():
     answerDiscount = 0.9
